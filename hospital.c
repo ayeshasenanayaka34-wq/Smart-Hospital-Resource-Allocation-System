60459b0070b719e78e7938a6393ca0ff94c9b27c
ayeshasenanayaka34-wg <ayesha.senanayaka34@gmail.com>
2026-09-17 20:28:11 +0530

    Add patient data arrays

--- hospital.c
+++ hospital.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include "hospital.h"
 
-int main(){
 char specialtyName[NUM_SPECIALTIES][50] = {
     "General Practice (OPD)",
     "Paediatrics",
@@ -53,4 +52,18 @@ int wardCapacity[NUM_WARDS] = {
 };
 
 int bedOccupancy[NUM_WARDS][MAX_BEDS];
-}
+char patientName[MAX_PATIENTS][50];
+int patientAge[MAX_PATIENTS];
+int urgencyLevel[MAX_PATIENTS];
+int specialtyID[MAX_PATIENTS];
+int wardID[MAX_PATIENTS];
+int daysAdmitted[MAX_PATIENTS];
+int registrationOrder[MAX_PATIENTS];
+int patientCount = 0;
+double baseFee[MAX_PATIENTS];
+double surcharge[MAX_PATIENTS];
+double wardCost[MAX_PATIENTS];
+double grossTotal[MAX_PATIENTS];
+double discount[MAX_PATIENTS];
+double finalPayable[MAX_PATIENTS];
+double waitingTime[MAX_PATIENTS];
